75404a38c33001c5ea05ca9db5cfd80594d3e0d9
Felista-Njeri <njeerikamau2@gmail.com>
2025-06-05 19:38:23 +0300

    changed wording from start locking to start earning

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -75,7 +75,7 @@ export default function Navbar() {
                 className="md:px-5 px-3 py-2 bg-amber-500 rounded-lg text-base font-semibold hover:bg-amber-500 hover:scale-105 transition-all flex items-center"
                 onClick={handleDash}
               >
-                Start Locking
+                Start Earning
                 <LockKeyhole className="ml-2 w-5 h-5 animate-pulse" />
               </motion.button>
             </div>
