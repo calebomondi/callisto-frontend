3088698ea9ad03db95bffe9065c21041ed1c7524
Felista-Njeri <njeerikamau2@gmail.com>
2025-06-16 09:37:11 +0300

    add shadcn components

---
+++ src/components/home/solutiontwo.tsx
@@ -0,0 +1,86 @@
+import { StickyScroll } from "../ui/sticky-scroll-reveal";
+import vaultDashboard from "/vault_dashboard.png";
+import { motion } from "framer-motion"
+const content = [
+  {
+    title: "Secure Vault",
+    description:
+      "Lock your assets in a secure, time-locked vault",
+    content: (
+      <div>
+        <img
+          src={vaultDashboard}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Vault Management",
+    description:
+      "Manage multiple vaults and assets easily",
+    content: (
+      <div>
+        <img
+          src={vaultDashboard}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Unlock Schedule",
+    description:
+      "Create custom unlock schedules for your assets",
+    content: (
+      <div>
+        <img
+          src={vaultDashboard}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+  {
+    title: "Asset Analytics",
+    description:
+      "Track ypur locked assets and upcoming unlocks",
+    content: (
+      <div>
+        <img
+          src={vaultDashboard}
+          className="h-full w-full object-cover"
+          alt="linear board demo"
+        />
+      </div>
+    ),
+  },
+];
+export function StickyScrollRevealDemo() {
+  return (
+    <div className="w-full py-4">
+        <div className="text-center">
+          <motion.h2
+            className="text-3xl font-bold mb-4"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Our Solutions
+          </motion.h2>
+          <motion.p
+            className="dark:text-gray-400 max-w-2xl mx-auto"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            Comprehensive tools to help you manage and secure your crypto assets
+          </motion.p>
+        </div>
+      <StickyScroll content={content} />
+    </div>
+  );
+}
